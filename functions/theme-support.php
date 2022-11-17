867152009f1f557b0b8834048de7ef3d64ccf5e6
Simon Jacks <simon@ape68.com>
2022-11-17 15:04:17 +0000

    Update theme-support.php

--- functions/theme-support.php
+++ functions/theme-support.php
@@ -103,9 +103,4 @@ function atelier_block_categories( $categories ) {
 }
 add_action( 'block_categories_all', 'atelier_block_categories', 10);
 
-// Allow SVG uploads
-function cc_mime_types($mimes) {
-  $mimes['svg'] = 'image/svg+xml';
-  return $mimes;
-}
-add_filter('upload_mimes', 'cc_mime_types');
+
