5bf4590c03df850687747d54b5aa382c3d06e247
Simon Jacks <simon@ape68.com>
2022-11-22 20:02:30 +0000

    Update text image

--- acf/load-points/blocks/text-image/text-image-block.php
+++ acf/load-points/blocks/text-image/text-image-block.php
@@ -22,60 +22,37 @@ $fields = get_field('text_image_fields');
 <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
     <?php 
         $image = $fields['image'];
-        $text_background = $fields['text_background'];
+        $button = $fields['button'];
     ?>
     <div class="grid-container">
         <div class="grid-x">
-            <?php if ($fields['image_position'] == "Left"): ?>
-                <div class="large-6 cell">
-                    <div class="image_text-image">
-                        <?php if ($fields['image_or_video'] == "video"): ?>
-                            <?php $video = $fields['video'] ?>
-                            <video autoplay>
-                                <source src="<?= $video['url'] ?>" type="video/mp4">
-                                Your browser does not support the video tag.
-                            </video>
-                        <?php else: ?>
-                            <?php echo wp_get_attachment_image( $image, 'full' ); ?>
-                        <?php endif ?>
-                    </div>
-                </div>
-            <?php endif; ?>
-                    
-                    
-
-            <div class="large-6 cell">
-                <div class="image_text-content">
-                    <?php if (!empty($text_background)): ?>
-                        <div class="image_text-content-background">
-                            <?php echo wp_get_attachment_image( $text_background, 'full' ); ?>
-                        </div>
+            <?php $order = ($fields['image_position'] == "Left") ? "order-1" : "order-3"; ?>
+            <div class="large-6 cell<?= $order ?>">
+                <div class="text-image-image">
+                    <?php if ($fields['image_or_video'] == "video"): ?>
+                        <?php $video = $fields['video'] ?>
+                        <video autoplay>
+                            <source src="<?= $video['url'] ?>" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+                    <?php else: ?>
+                        <?php echo wp_get_attachment_image( $image, 'full' ); ?>
                     <?php endif ?>
-                    <div class="image_text-content-text">
+                </div>
+            </div>
+
+            <div class="large-6 cell order-2">
+                <div class="text-image-content">
+                    <div class="text-image-content-text">
                         <h3><?= $fields['title'] ?></h3>
-                        <p class=""><?= $fields['body'] ?></p>
+                        <?= $fields['body'] ?>
                         <?php if (!empty($fields['button_title'])): ?>
-                            <p class="controls"><a href="<?= $fields['button_link'] ?>" class="button black bottom-left"><?= $fields['button_title'] ?></a></p>
+                            <p class="controls"><a href="<?= $button['url'] ?>" class="button black bottom-left"><?= $button['title'] ?></a></p>
                         <?php endif ?>
                     </div>
                 </div>
             </div>
 
-            <?php if ($fields['image_position'] == "Right"): ?>
-                <div class="large-6 cell">
-                    <div class="image_text-image">
-                        <?php if ($fields['image_or_video'] == "video"): ?>
-                            <?php $video = $fields['video'] ?>
-                            <video autoplay>
-                                <source src="<?= $video['url'] ?>" type="video/mp4">
-                                Your browser does not support the video tag.
-                            </video>
-                        <?php else: ?>
-                            <?php echo wp_get_attachment_image( $image, 'full' ); ?>
-                        <?php endif ?>
-                    </div>
-                </div>
-            <?php endif ?>
         </div>
     </div>
 </section>
