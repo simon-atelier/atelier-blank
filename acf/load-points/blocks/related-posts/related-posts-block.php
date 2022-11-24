ee55511770880f371486552a27b7ca4e8fbca252
Simon Jacks <simon@ape68.com>
2022-11-24 14:34:33 +0000

    Updated related posts

--- acf/load-points/blocks/related-posts/related-posts-block.php
+++ acf/load-points/blocks/related-posts/related-posts-block.php
@@ -16,7 +16,10 @@ if( !empty($block['align']) ) {
 }
 
 $posts = get_field('related_posts');
-
+if (empty($posts)) {
+    $args = array( 'posts_per_page' => 4, 'post_type'=> 'post' );
+    $posts = get_posts( $args );
+}
 ?>
 
 <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
@@ -33,28 +36,28 @@ $posts = get_field('related_posts');
                     $category = get_the_category($post->ID);
                 ?>
                 <div class="large-4 cell">
-                    <div class="related_post">
-                        <div class="related_post-image">
+                    <div class="related-posts">
+                        <div class="related-posts-image">
                             <?php echo wp_get_attachment_image( $image, 'full'); ?>
                         </div>
-                        <div class="related_post-content">
+                        <div class="related-posts-content">
                             <span class="tag"><?= $category[0]->name ?></span>
                             <h3 class="white"><?php echo get_the_title($post->ID); ?></h3>
-                            <p><?php echo strip_limit($post->post_content, 100); ?></p>
-                            <div class="related_post-content-controls">
+                            <p><?php echo get_the_excerpt($post->ID); ?></p>
+                            <div class="related-posts-content-controls">
                                 <a href="<?php echo get_the_permalink($post->ID); ?>" class="button black bottom-left">Read more</a>
                             </div>
                         </div>
                     </div>
                 </div>
-            <?php endforeach ?>
+            <?php endforeach; wp_reset_postdata(); ?>
         </div>
         <div class="swiper_icon">
             <img src="<?php bloginfo('template_directory'); ?>/assets/images/swipe.svg" alt="Swipe">
         </div>
         <div class="grid-x">
             <div class="large-12 cell">
-                <div class="related_post-controls">
+                <div class="related-posts-controls">
                     <p class="midtext"><a href="<?php bloginfo('url'); ?>/" class="button bottom-left red">See all posts</a></p>
                 </div>
             </div>
