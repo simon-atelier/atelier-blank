fe02a7d22c727f1c0596909f4f4f436d23ac4a43
Simon Jacks <simon@ape68.com>
2022-12-07 18:33:40 +0000

    Text-image update

--- acf/load-points/blocks/text-image/text-image-block.php
+++ acf/load-points/blocks/text-image/text-image-block.php
@@ -30,11 +30,7 @@ $fields = get_field('text_image_fields');
             <div class="large-6 cell<?= $order ?>">
                 <div class="text-image-image">
                     <?php if ($fields['image_or_video'] == "video"): ?>
-                        <?php $video = $fields['video'] ?>
-                        <video autoplay>
-                            <source src="<?= $video['url'] ?>" type="video/mp4">
-                            Your browser does not support the video tag.
-                        </video>
+                        <?php if (!empty($fields['video'])) echo $fields['video']; ?>
                     <?php else: ?>
                         <?php echo wp_get_attachment_image( $image, 'full' ); ?>
                     <?php endif ?>
