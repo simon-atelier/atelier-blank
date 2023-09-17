6986b9655593ae38b10548baee87da0b747f7de2
Simon Jacks <simon@ape68.com>
2023-09-17 19:16:13 +0100

    All blocks switched to block.json

--- acf/acf_blocks.php
+++ acf/acf_blocks.php
@@ -1,22 +1,10 @@
 <?php
-add_action('acf/init', 'flex_acf_init_block_types');
-function flex_acf_init_block_types() {
-    if( function_exists('acf_register_block_type') ) {
-        acf_register_block_type(array(
-            'name' => 'content-container',
-            'title' => __('Content container'),
-            'description' => __('Wraps content in a styled container.'), 
-            'category' => 'flexi-blocks',
-            'icon' => 'welcome-widgets-menus',
-            'keywords' => [ 'container', 'content', 'box' ],
-            'supports' => [
-                'align' => true,
-                'mode' => false,
-                'jsx' => true
-            ],
-            'render_template' => get_template_directory() . '/acf/blocks/content-container/content-container-block.php',
-            'enqueue_style'   => get_template_directory_uri() . '/acf/blocks/content-container/content-container.css'
-        ));
-   
-    } 
+function at_load_blocks(){
+    $path = parse_url(get_bloginfo('url'), PHP_URL_PATH);
+    $get_block_directories = scandir($_SERVER['DOCUMENT_ROOT'] . $path . '/wp-content/themes/atelier-blank/acf/blocks');
+    foreach ($get_block_directories as $key => $block_directory) {
+        if ($block_directory[0] == ".") continue;
+        register_block_type(__DIR__. '/blocks/' . $block_directory);
+    }
 }
+add_action( 'init', 'at_load_blocks' );
